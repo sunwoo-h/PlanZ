b910ae4eb43e9e2db26b407ce915eaf3ea2afb90
sunwoo-h <flexible8011@gmail.com>
2025-06-22 17:59:03 +0900

    feat: InitPage 구현

--- src/components/pages/InitPage.jsx
+++ src/components/pages/InitPage.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import Button from "../ui/Button";
 import styled from "styled-components";
+import logo from "../../assets/PlanZLogo.png";
+
+const MainContainer = styled.div`
+  margin: 200px auto;
+`;
+
+const SubContainer = styled.div`
+  display: flex;
+  gap: 40px;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const ButtonWrapper = styled.div`
   width: 257px;
@@ -12,11 +24,18 @@ const ButtonWrapper = styled.div`
 const InitPage = () => {
   return (
     <div>
-      나만의 일정 관리 서비스
-      <ButtonWrapper>
-        <Button title={"로그인"} color={"black"} />
-        <Button title={"회원가입"} color={"white"} />
-      </ButtonWrapper>
+      <MainContainer>
+        <SubContainer>
+          <img src={logo} alt="PlanZ 로고" />
+          <div style={{ fontSize: "32px", fontWeight: "bold" }}>
+            나만의 일정 관리 서비스
+          </div>
+          <ButtonWrapper>
+            <Button title={"로그인"} color={"black"} />
+            <Button title={"회원가입"} color={"white"} />
+          </ButtonWrapper>
+        </SubContainer>
+      </MainContainer>
     </div>
   );
 };
