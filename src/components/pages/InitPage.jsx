30bc948594c6b4085633f41f1a6ddff101e4c10a
sunwoo-h <flexible8011@gmail.com>
2025-06-22 17:43:20 +0900

    feat: Button 컴포넌트 구현2

--- src/components/pages/InitPage.jsx
+++ src/components/pages/InitPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Button from "../ui/Button";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  width: 257px;
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+`;
 
 const InitPage = () => {
   return (
     <div>
       나만의 일정 관리 서비스
-      <Button title={"로그인"} color={"black"} />
+      <ButtonWrapper>
+        <Button title={"로그인"} color={"black"} />
+        <Button title={"회원가입"} color={"white"} />
+      </ButtonWrapper>
     </div>
   );
 };
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.div`
-  border: solid 1px black;
   background-color: ${({ color }) =>
     color === "white" ? "#fff" : color === "black" ? "#000" : "#ccc"};
   color: ${({ color }) =>
