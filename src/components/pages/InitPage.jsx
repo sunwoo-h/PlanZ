78acd95ba43475adc705de987ce3fe483734a764
sunwoo-h <flexible8011@gmail.com>
2025-06-22 17:31:01 +0900

    feat: Button 컴포넌트 구현

--- src/components/pages/InitPage.jsx
+++ src/components/pages/InitPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "../ui/Button";
 
 const InitPage = () => {
-  return <div>나만의 일정 관리 서비스</div>;
+  return (
+    <div>
+      나만의 일정 관리 서비스
+      <Button title={"로그인"} color={"black"} />
+    </div>
+  );
 };
 
 export default InitPage;
---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.div`
+  border: solid 1px black;
+  background-color: ${({ color }) =>
+    color === "white" ? "#fff" : color === "black" ? "#000" : "#ccc"};
+  color: ${({ color }) =>
+    color === "white" ? "#000" : color === "black" ? "#fff" : "#ccc"};
+  border-radius: 10px;
+  cursor: pointer;
+  width: 100%;
+  padding: 14px 0px;
+  text-align: center;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25);
+`;
+
+const Button = ({ title, color }) => {
+  return <StyledButton color={color}>{title}</StyledButton>;
+};
+
+export default Button;
