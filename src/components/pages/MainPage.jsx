8a2b4827c1190127e07f72b9c0ed3ef9f45806d9
sunwoo-h <flexible8011@gmail.com>
2025-06-24 16:21:59 +0900

    feat: 메인페이지 초기 구현

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,7 +1,130 @@
 import React from "react";
+import logo from "../../assets/PlanZlogo.png";
+import styled from "styled-components";
+import Button from "../ui/Button";
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+const GlassCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  padding: 30px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  background-image: linear-gradient(
+    to bottom right,
+    rgba(255, 255, 255, 0.7) 0%,
+    rgba(255, 255, 255, 0.2) 50%,
+    rgba(255, 255, 255, 0.7) 100%
+  );
+`;
+
+const InputBox = styled.textarea`
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.25);
+  padding: 15px;
+  opacity: 0.8;
+  border: none;
+  outline: none;
+  resize: none;
+  box-sizing: border-box;
+`;
+
+const TodoBox = styled.div`
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.25);
+  padding: 15px;
+  border: none;
+`;
+
+const StyledButton = styled.div`
+  background-color: ${({ color }) =>
+    color === "white" ? "#fff" : color === "black" ? "#000" : "#ccc"};
+  color: ${({ color }) =>
+    color === "white" ? "#000" : color === "black" ? "#fff" : "#ccc"};
+  border-radius: 10px;
+  cursor: pointer;
+  width: 30px;
+  padding: 5px 7px;
+  text-align: center;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25);
+  font-size: 0.8rem;
+`;
+
+const BigNumber = styled.div`
+  text-align: center;
+  font-size: 70px;
+`;
 
 const MainPage = () => {
-  return <div>메인페이지입니다.</div>;
+  return (
+    <div>
+      <Row style={{ justifyContent: "space-between" }}>
+        <img style={{ width: "100px" }} src={logo} alt="PlanZ 로고" />
+        <div>UserID님 안녕하세요!</div>
+      </Row>
+      <Row style={{ gap: "40px" }}>
+        <Column>
+          <GlassCard>달력</GlassCard>
+
+          <GlassCard>
+            <InputBox placeholder="할일을 추가해보세요" />
+            <Button title={"추가하기"} color={"black"} />
+          </GlassCard>
+        </Column>
+        <Column>
+          <GlassCard>
+            2032년 9월 9일의 투두리스트
+            <TodoBox>
+              <Row
+                style={{
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                투두리스트1
+                <Row style={{ gap: "10px" }}>
+                  <StyledButton color="black">완료</StyledButton>
+                  <StyledButton color="white">삭제</StyledButton>
+                </Row>
+              </Row>
+            </TodoBox>
+          </GlassCard>
+          <Row style={{ gap: "20px" }}>
+            <GlassCard>
+              <div style={{ textAlign: "center", fontWeight: "bold" }}>
+                남은 할 일
+              </div>
+              <BigNumber>5</BigNumber>
+            </GlassCard>
+            <GlassCard>
+              <div style={{ textAlign: "center", fontWeight: "bold" }}>
+                완료된 할 일
+              </div>
+              <BigNumber>5</BigNumber>
+            </GlassCard>
+            <GlassCard>
+              <div style={{ textAlign: "center", fontWeight: "bold" }}>
+                목표 달성률
+              </div>
+              <BigNumber>100%</BigNumber>
+            </GlassCard>
+          </Row>
+        </Column>
+      </Row>
+    </div>
+  );
 };
 
 export default MainPage;
